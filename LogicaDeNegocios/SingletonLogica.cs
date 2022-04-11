c834dc4ca9c3aff9c5308dff93487ebf0d62cd97
Msotelo1 <msotelo111201@gmail.com>
2022-04-10 21:32:48 -0300

    Cambios Persistenci y Logica

--- LogicaDeNegocios/SingletonLogica.cs
+++ LogicaDeNegocios/SingletonLogica.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entidades;
 
 namespace LogicaDeNegocios
 {
     public sealed class SingletonLogica
     {
+        public List<Persona> Personas = new List<Persona>();
+        public List<Cobertura> Coberturas = new List<Cobertura>();
+        public List<Enfermedad> Enfermedades = new List<Enfermedad>();
+        public List<Atencion> Atenciones = new List<Atencion>();
+
         private static SingletonLogica instancia = null;
         private SingletonLogica() { }
         public static SingletonLogica Instancia
@@ -22,5 +28,20 @@ namespace LogicaDeNegocios
                 return instancia;
             }
         }
+        public Enfermedad BuscarEnfermedad(int codigoEnfermedad)
+        {
+            Enfermedad enfermedad = (Enfermedad)Enfermedades.Where(x => x.Codigo == codigoEnfermedad);
+            return enfermedad;
+        }
+        public double ValidarEnfermedad(Persona persona, Cobertura cobertura, int codigoEnfermedad, DateTime fecha)
+        {
+            if (persona.CoberturaAsociada.EnfermedadesIncluidadas.Contains(BuscarEnfermedad(codigoEnfermedad)))
+            {
+                Atencion atencion = new Atencion(Atenciones.Count(), fecha, BuscarEnfermedad(codigoEnfermedad), persona);
+                return BuscarEnfermedad(codigoEnfermedad).Costo;
+            }
+            return 0;
+        }
     }
+
 }
