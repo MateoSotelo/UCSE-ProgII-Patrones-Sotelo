41dc617ae7646cebb9b1ffe2778e4b4ae83e2d18
Msotelo1 <msotelo111201@gmail.com>
2022-04-12 15:57:52 -0300

    .

---
+++ Entidades/Listas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Listas
+    {
+        public List<Persona> Personas { get; set; }
+        public List<Atencion> Atenciones { get; set; }
+        public List<Cobertura> Coberturas { get; set; }
+        public List<Enfermedad> Enfermedades { get; set; }
+        public Listas(List<Persona> personas = null, List<Atencion> atenciones = null, List<Cobertura> coberturas = null, List<Enfermedad> enfermedades = null)
+        {
+            this.Personas = personas; ;
+            this.Atenciones = atenciones;
+            this.Coberturas = coberturas;
+            this.Enfermedades = enfermedades;
+        }
+        
+    }
+}
