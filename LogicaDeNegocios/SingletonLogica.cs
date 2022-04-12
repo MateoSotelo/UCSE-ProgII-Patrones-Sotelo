4197bc48a62e955a9ae709ad7d883a3c6dd96147
Msotelo1 <msotelo111201@gmail.com>
2022-04-12 15:46:31 -0300

    Lista de entidades

--- LogicaDeNegocios/SingletonLogica.cs
+++ LogicaDeNegocios/SingletonLogica.cs
@@ -9,10 +9,7 @@ namespace LogicaDeNegocios
 {
     public sealed class SingletonLogica
     {
-        public List<Persona> Personas = new List<Persona>();
-        public List<Cobertura> Coberturas = new List<Cobertura>();
-        public List<Enfermedad> Enfermedades = new List<Enfermedad>();
-        public List<Atencion> Atenciones = new List<Atencion>();
+        public Listas listas = new Listas();
 
         private static SingletonLogica instancia = null;
         private SingletonLogica() { }
@@ -30,14 +27,14 @@ namespace LogicaDeNegocios
         }
         public Enfermedad BuscarEnfermedad(int codigoEnfermedad)
         {
-            Enfermedad enfermedad = (Enfermedad)Enfermedades.Where(x => x.Codigo == codigoEnfermedad);
+            Enfermedad enfermedad = (Enfermedad)listas.Enfermedades.Where(x => x.Codigo == codigoEnfermedad);
             return enfermedad;
         }
         public double ValidarEnfermedad(Persona persona, Cobertura cobertura, int codigoEnfermedad, DateTime fecha)
         {
             if (persona.CoberturaAsociada.EnfermedadesIncluidadas.Contains(BuscarEnfermedad(codigoEnfermedad)))
             {
-                Atencion atencion = new Atencion(Atenciones.Count(), fecha, BuscarEnfermedad(codigoEnfermedad), persona);
+                Atencion atencion = new Atencion(listas.Atenciones.Count(), fecha, BuscarEnfermedad(codigoEnfermedad), persona);
                 return BuscarEnfermedad(codigoEnfermedad).Costo;
             }
             return 0;
