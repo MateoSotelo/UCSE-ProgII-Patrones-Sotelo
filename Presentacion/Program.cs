1d4cef97209ae0186bb1c8f95f9b470c66b01c7d
Msotelo1 <msotelo111201@gmail.com>
2022-04-12 15:57:41 -0300

    Comienzo presentacion

--- Presentacion/Program.cs
+++ Presentacion/Program.cs
@@ -5,9 +5,22 @@ namespace Presentacion
 {
     class Program
     {
+        SingletonLogica logica = SingletonLogica.Instancia;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Carga de una nueva atencion");
+            
+        }
+
+        public void CargarAtencion()
+        {
+            Console.WriteLine("Nueva atencion");
+            int CodigoEnfermedad = 0;
+            
+            Console.WriteLine("Ingrese codigo de la enfermedad");
+            CodigoEnfermedad = int.Parse(Console.ReadLine());
+            
+
         }
     }
 }
